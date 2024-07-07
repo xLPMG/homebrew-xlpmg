3a752f003afff221021b44f90dc3c53237833a89
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-07-07 21:40:24 +0200

    feat: add libresprite

---
+++ Casks/l/libresprite.rb
@@ -0,0 +1,11 @@
+cask 'libesprite' do
+  version '1.0'
+  sha256 '85004254166204291a9c08007592cb6d38ac89a01faec3aa37c037a39d86b9eb'
+
+  url 'https://github.com/LibreSprite/LibreSprite/releases/download/v1.0/LibreSprite-macos-x86_64.zip'
+  name 'Libresprite'
+  desc 'A free and open source pixel art program'
+  homepage 'https://libresprite.github.io'
+
+  app 'Libresprite.app'
+end
